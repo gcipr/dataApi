3107b54b7eaec16fbe92c468f90d8b8a915efa98
DESKTOP-RFH0DUE\gcipr <g.cipriangheorghe@gmail.com>
2020-03-07 08:54:16 +0000

    Added the entire library of code

---
+++ Data.Web/Utils/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Data.Domain.Entities;
+using Data.Domain.EntitiesDTO;
+
+namespace Data.Web.Utils
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Add as many of these lines as you need to map your objects
+            CreateMap<FishingSpot, FishingSpotDTO>();
+            CreateMap<FishingSpotDTO, FishingSpot>();
+        }
+    }
+}
